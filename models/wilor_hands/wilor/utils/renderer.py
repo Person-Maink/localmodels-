ca4a5591a5048d97a025292b3cba81ba47512232
Person-Maink <mamayankt.2002@gmail.com>
2026-02-11 22:57:40 +0000

    same pls

--- models/wilor_hands/wilor/utils/renderer.py
+++ models/wilor_hands/wilor/utils/renderer.py
@@ -102,36 +102,36 @@ def rotz(theta):
     )
     
 
-def create_raymond_lights() -> List[pyrender.Node]:
-    """
-    Return raymond light nodes for the scene.
-    """
-    thetas = np.pi * np.array([1.0 / 6.0, 1.0 / 6.0, 1.0 / 6.0])
-    phis = np.pi * np.array([0.0, 2.0 / 3.0, 4.0 / 3.0])
+# def create_raymond_lights() -> List[pyrender.Node]:
+#     """
+#     Return raymond light nodes for the scene.
+#     """
+#     thetas = np.pi * np.array([1.0 / 6.0, 1.0 / 6.0, 1.0 / 6.0])
+#     phis = np.pi * np.array([0.0, 2.0 / 3.0, 4.0 / 3.0])
 
-    nodes = []
+#     nodes = []
 
-    for phi, theta in zip(phis, thetas):
-        xp = np.sin(theta) * np.cos(phi)
-        yp = np.sin(theta) * np.sin(phi)
-        zp = np.cos(theta)
+#     for phi, theta in zip(phis, thetas):
+#         xp = np.sin(theta) * np.cos(phi)
+#         yp = np.sin(theta) * np.sin(phi)
+#         zp = np.cos(theta)
 
-        z = np.array([xp, yp, zp])
-        z = z / np.linalg.norm(z)
-        x = np.array([-z[1], z[0], 0.0])
-        if np.linalg.norm(x) == 0:
-            x = np.array([1.0, 0.0, 0.0])
-        x = x / np.linalg.norm(x)
-        y = np.cross(z, x)
+#         z = np.array([xp, yp, zp])
+#         z = z / np.linalg.norm(z)
+#         x = np.array([-z[1], z[0], 0.0])
+#         if np.linalg.norm(x) == 0:
+#             x = np.array([1.0, 0.0, 0.0])
+#         x = x / np.linalg.norm(x)
+#         y = np.cross(z, x)
 
-        matrix = np.eye(4)
-        matrix[:3,:3] = np.c_[x,y,z]
-        nodes.append(pyrender.Node(
-            light=pyrender.DirectionalLight(color=np.ones(3), intensity=1.0),
-            matrix=matrix
-        ))
+#         matrix = np.eye(4)
+#         matrix[:3,:3] = np.c_[x,y,z]
+#         nodes.append(pyrender.Node(
+#             light=pyrender.DirectionalLight(color=np.ones(3), intensity=1.0),
+#             matrix=matrix
+#         ))
 
-    return nodes
+#     return nodes
 
 class Renderer:
 
