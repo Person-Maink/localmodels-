47c0bd6cf28d8c395cf6a417651236170099c4ec
Person-Maink <mamayankt.2002@gmail.com>
2026-02-11 22:51:46 +0000

    removed renderer from wilor __init__

--- models/wilor_hands/wilor/models/wilor.py
+++ models/wilor_hands/wilor/models/wilor.py
@@ -17,7 +17,7 @@ log = get_pylogger(__name__)
 
 class WiLoR(pl.LightningModule):
 
-    def __init__(self, cfg: CfgNode, init_renderer: bool = True):
+    def __init__(self, cfg: CfgNode, init_renderer: bool = False):
         """
         Setup WiLoR model
         Args:
