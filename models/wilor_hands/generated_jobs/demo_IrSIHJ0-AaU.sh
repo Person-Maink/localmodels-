6b2555c8087149f59594aa5204db5cb3c034c8b3
Mayank Thakur <mthakur@login02.delftblue.tudelft.nl>
2026-03-31 17:03:13 +0200

    more generated jobs dei

---
+++ models/wilor_hands/generated_jobs/demo_IrSIHJ0-AaU.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+# ================ SLURM SETUP ================
+# Available HPC Partitions:
+#   compute / compute-p1   : CPU jobs (48 CPUs, 185 GB RAM, Phase 1)
+#   compute-p2             : CPU jobs (64 CPUs, 250 GB RAM, Phase 2)
+#   gpu / gpu-v100         : GPU jobs (4x V100, 32 GB VRAM each, Phase 1)
+#   gpu-a100               : GPU jobs (4x A100, 80 GB VRAM each, Phase 2)
+#   gpu-a100-small         : Small GPU jobs (≤1 GPU, ≤10 GB VRAM, ≤2 CPUs, ≤4h)
+#   memory                 : High-memory CPU jobs (>250 GB RAM)
+#   visual                 : Visualization jobs
+
+#SBATCH --job-name=wilor-inference
+#SBATCH --partition=gpu-a100-small
+#SBATCH --time=00:07:09
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=2
+#SBATCH --gpus-per-task=1
+#SBATCH --mem-per-gpu=10G
+#SBATCH --account=Education-EEMCS-MSc-DSAIT
+#SBATCH --output=%x.out
+
+# ================ OUTPUT FILES ================
+# compute a small incremental index based on existing files
+base_name="${SLURM_JOB_NAME}"
+dir="SLURM_logs"
+mkdir -p "$dir"
+count=$(printf "%03d" $(($(ls "$dir" 2>/dev/null | grep -c "^${base_name}_[0-9]\+\.out$") + 1)))
+outfile="${dir}/${base_name}_${count}.out"
+
+# redirect stdout and stderr
+exec >"$outfile" 2>&1
+
+# ================ SLURM SETUP ================
+# Load modules:
+module load 2024r1
+# module load miniconda3
+module load cuda/11.7
+#module load openmpi/4.1.4
+#module load py-torch/1.12.1
+#module load py-pip/22.2.2
+# module load py-numpy
+# module load py-pyyaml
+# module load py-tqdm
+# module load ffmpeg
+
+# ================ CODE EXECUTION ================
+echo "Loaded modules:"
+module list 2>&1
+
+
+echo "================ SLURM JOB INFO ================"
+echo "Job ID:         $SLURM_JOB_ID"
+echo "Job Name:       $SLURM_JOB_NAME"
+echo "Partition:      $SLURM_JOB_PARTITION"
+echo "Node List:      $SLURM_JOB_NODELIST"
+echo "CPUs per task:  $SLURM_CPUS_PER_TASK"
+echo "Memory per CPU: $SLURM_MEM_PER_CPU"
+echo "GPUs per task:  $SLURM_GPUS_PER_TASK"
+echo "Memory per GPU: $SLURM_MEM_PER_GPU"
+echo "Submit dir:     $SLURM_SUBMIT_DIR"
+echo "Work dir:       $(pwd)"
+echo "Job started at: $(date)"
+start_time=$(date +%s)
+echo "==============================================="
+
+mkdir -p "/scratch/mthakur/manifold/data/images/IrSIHJ0-AaU_frames"
+
+srun apptainer exec \
+  --nv \
+  --bind /scratch:/scratch \
+  /scratch/mthakur/manifold/models/wilor_hands/apptainer/template.sif \
+  python /scratch/mthakur/manifold/models/wilor_hands/main.py --video "IrSIHJ0-AaU"
+
+echo "==============================================="
+end_time=$(date +%s)
+elapsed=$(( end_time - start_time ))
+hours=$(printf "%.2f" "$(echo "$elapsed/3600" | bc -l)")
+
+echo "Job finished at: $(date)"
+echo "Execution took $hours hours"
+echo "Writing output to $outfile"
+echo "==============================================="
